718a10f5199699b85f22afb4e45f96c858619558
Thomas Kaczmarek <mulenatic@gmail.com>
2025-02-12 10:54:36 +0100

    Test is also using the application property.

--- org.acme.quarkus-in-action/src/test/java/org/acme/GreetingResourceTest.java
+++ org.acme.quarkus-in-action/src/test/java/org/acme/GreetingResourceTest.java
@@ -6,15 +6,21 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 @QuarkusTest
 class GreetingResourceTest {
+
+    @ConfigProperty(name="greeting")
+    String greeting;
+
     @Test
     void testHelloEndpoint() {
         given()
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("Hello from Quarkus REST"));
+             .body(is(greeting));
     }
 
 }
