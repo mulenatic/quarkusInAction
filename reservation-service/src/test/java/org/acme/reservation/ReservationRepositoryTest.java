b9741f67bf6c2a614f18dced52dfd7cf33a5b314
Thomas Kaczmarek <mulenatic@gmail.com>
2025-02-19 07:44:40 +0100

    Using Rest in a new Test

--- reservation-service/src/test/java/org/acme/reservation/ReservationRepositoryTest.java
+++ reservation-service/src/test/java/org/acme/reservation/ReservationRepositoryTest.java
@@ -1,32 +1,43 @@
 package org.acme.reservation;
 
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.net.URL;
 import java.time.LocalDate;
 
 import org.acme.reservation.reservation.Reservation;
-import org.acme.reservation.reservation.ReservationsRepository;
-import org.junit.jupiter.api.Assertions;
+import org.acme.reservation.rest.ReservationResource;
 import org.junit.jupiter.api.Test;
 
-
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
-import jakarta.inject.Inject;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 
 @QuarkusTest
 public class ReservationRepositoryTest {
 
-  @Inject
-  ReservationsRepository repository;
+  @TestHTTPEndpoint(ReservationResource.class)
+  @TestHTTPResource
+  URL reservationResource;
 
   @Test
-  public void testCreateReservation() {
+  public void testReservationIds() {
     Reservation reservation = new Reservation();
     reservation.startDay = LocalDate.now().plusDays(5);
     reservation.endDay = LocalDate.now().plusDays(12);
     reservation.carId = 384L;
-    repository.save(reservation);
 
-    Assertions.assertNotNull(reservation);
-    Assertions.assertTrue(repository.findAll().contains(reservation));
+    RestAssured
+        .given()
+        .contentType(ContentType.JSON)
+        .body(reservation)
+        .when()
+        .post(reservationResource)
+        .then()
+        .statusCode(200)
+        .body("id", notNullValue());
   }
 
 }
