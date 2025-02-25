b5c9e159679a9ec7275ff23901c188f88828dd1a
Thomas Kaczmarek <mulenatic@gmail.com>
2025-02-25 14:42:09 +0100

    Added components for availablecars and listofreservations

--- users-service/src/main/java/org/acme/users/ReservationsResource.java
+++ users-service/src/main/java/org/acme/users/ReservationsResource.java
@@ -1,14 +1,20 @@
 package org.acme.users;
 
 import java.time.LocalDate;
+import java.util.Collection;
 
+import org.acme.users.model.Car;
+import org.acme.users.model.Reservation;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.resteasy.reactive.RestForm;
 import org.jboss.resteasy.reactive.RestQuery;
+import org.jboss.resteasy.reactive.RestResponse;
 
 import io.quarkus.qute.CheckedTemplate;
 import io.quarkus.qute.TemplateInstance;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -23,6 +29,14 @@ public class ReservationsResource {
         LocalDate startDate,
         LocalDate endDate,
         String name);
+
+    public static native TemplateInstance listofreservations(
+        Collection<Reservation> reservations);
+
+    public static native TemplateInstance availablecars(
+        Collection<Car> cars,
+        LocalDate startDate,
+        LocalDate endDate);
   }
 
   @Inject
@@ -44,6 +58,41 @@ public class ReservationsResource {
 
     return Templates.index(startDate, enddDate, securityContext.getUserPrincipal().getName());
 
+  }
+
+  @GET
+  @Produces(MediaType.TEXT_HTML)
+  @Path("/get")
+  public TemplateInstance getResevations() {
+    Collection<Reservation> reservations = client.allReservations();
+    return Templates.listofreservations(reservations);
+  }
+
+  @GET
+  @Produces(MediaType.TEXT_HTML)
+  @Path("/available")
+  public TemplateInstance getAvailableCars(@RestQuery LocalDate startDate, @RestQuery LocalDate endDate) {
+    Collection<Car> availableCars = client.availability(startDate, endDate);
+    return Templates.availablecars(availableCars, startDate, endDate);
+  }
+
+  @POST
+  @Produces(MediaType.TEXT_HTML)
+  @Path("/reserve")
+  public RestResponse<TemplateInstance> create(
+      @RestForm LocalDate startDate,
+      @RestForm LocalDate endDate,
+      @RestForm Long carId) {
+    Reservation reservation = new Reservation();
+    reservation.startDay = startDate;
+    reservation.endDay = endDate;
+    reservation.carId = carId;
+    client.make(reservation);
+
+    return RestResponse.ResponseBuilder
+        .ok(getResevations())
+        .header("HX-Trigger-After-Swap", "update-available-cars-list")
+        .build();
   }
 
 }
