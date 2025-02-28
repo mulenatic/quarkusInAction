1eca801b6dd80afc78e5c2853bac0cdfd3ca94a1
Thomas Kaczmarek <mulenatic@gmail.com>
2025-02-28 11:13:00 +0100

    Made Car an JPA Entity

--- inventory-service/src/main/java/org/acme/inventory/model/Car.java
+++ inventory-service/src/main/java/org/acme/inventory/model/Car.java
@@ -1,10 +1,99 @@
 package org.acme.inventory.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Car {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   public Long id;
   public String licensePlateNumber;
   public String manufacturer;
   public String model;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getLicensePlateNumber() {
+    return licensePlateNumber;
+  }
+
+  public void setLicensePlateNumber(String licensePlateNumber) {
+    this.licensePlateNumber = licensePlateNumber;
+  }
+
+  public String getManufacturer() {
+    return manufacturer;
+  }
+
+  public void setManufacturer(String manufacturer) {
+    this.manufacturer = manufacturer;
+  }
+
+  public String getModel() {
+    return model;
+  }
+
+  public void setModel(String model) {
+    this.model = model;
+  }
+
+  @Override
+  public String toString() {
+    return "Car [id=" + id + ", licensePlateNumber=" + licensePlateNumber + ", manufacturer=" + manufacturer
+        + ", model=" + model + "]";
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    result = prime * result + ((licensePlateNumber == null) ? 0 : licensePlateNumber.hashCode());
+    result = prime * result + ((manufacturer == null) ? 0 : manufacturer.hashCode());
+    result = prime * result + ((model == null) ? 0 : model.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Car other = (Car) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    if (licensePlateNumber == null) {
+      if (other.licensePlateNumber != null)
+        return false;
+    } else if (!licensePlateNumber.equals(other.licensePlateNumber))
+      return false;
+    if (manufacturer == null) {
+      if (other.manufacturer != null)
+        return false;
+    } else if (!manufacturer.equals(other.manufacturer))
+      return false;
+    if (model == null) {
+      if (other.model != null)
+        return false;
+    } else if (!model.equals(other.model))
+      return false;
+    return true;
+  }
 
 }
