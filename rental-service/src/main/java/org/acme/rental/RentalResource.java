51827c6910d58a27f8c11948e164e775db18ca5e
Thomas Kaczmarek <mulenatic@gmail.com>
2025-03-13 15:22:35 +0100

    Using Mongodb Panache with active record pattern in rental service.

--- rental-service/src/main/java/org/acme/rental/RentalResource.java
+++ rental-service/src/main/java/org/acme/rental/RentalResource.java
@@ -1,9 +1,16 @@
 package org.acme.rental;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
+import org.acme.rental.entity.Rental;
+
+import io.quarkus.logging.Log;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -12,22 +19,50 @@ import jakarta.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 public class RentalResource {
 
-  private final RentalsRepository rentalsRepository;
-
-  public RentalResource(RentalsRepository rentalsRepository) {
-    this.rentalsRepository = rentalsRepository;
-  }
-
   @Path("/start/{userId}/{reservationId}")
   @POST
   @Produces(MediaType.APPLICATION_JSON)
   public Rental start(String userId, Long reservationId) {
-    return rentalsRepository.create(userId, reservationId);
+    Log.infof("Starting rental for %s with reservation %s", userId, reservationId);
+
+    Rental rental = new Rental();
+    rental.userId = userId;
+    rental.reservationId = reservationId;
+    rental.startDate = LocalDate.now();
+    rental.active = true;
+
+    rental.persist();
+    return rental;
+  }
+
+  @PUT
+  @Path("/end/{userId}/{reservationId}")
+  public Rental end(String userId, Long reservationId) {
+    Log.infof("Ending rental for %s with reservation %s", userId, reservationId);
+
+    Optional<Rental> optionalRental = Rental.findByUserAndReservationIdsOptional(userId, reservationId);
+
+    if (optionalRental.isPresent()) {
+      Rental rental = optionalRental.get();
+      rental.endDate = LocalDate.now();
+      rental.active = false;
+      rental.update();
+      return rental;
+    } else {
+      throw new NotFoundException("Rental not found");
+    }
+
   }
 
   @GET
   public List<Rental> getAll() {
-    return rentalsRepository.getAll();
+    return Rental.listAll();
+  }
+
+  @GET
+  @Path("/active")
+  public List<Rental> listActive() {
+    return Rental.listActive();
   }
 
 }
