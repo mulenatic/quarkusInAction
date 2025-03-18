6b500538510d778eabda034959d0f16b2b8caf88
Thomas Kaczmarek <mulenatic@gmail.com>
2025-03-18 07:54:44 +0100

    Remeove MongoEntity annotation from Invoice and added toString methods
    Still not working correctly.

--- billing-service/src/main/java/org/acme/billing/model/Invoice.java
+++ billing-service/src/main/java/org/acme/billing/model/Invoice.java
@@ -3,9 +3,7 @@ package org.acme.billing.model;
 import java.time.LocalDate;
 
 import io.quarkus.mongodb.panache.PanacheMongoEntity;
-import io.quarkus.mongodb.panache.common.MongoEntity;
 
-@MongoEntity
 public class Invoice extends PanacheMongoEntity {
 
     public double totalPrice;
@@ -17,14 +15,25 @@ public class Invoice extends PanacheMongoEntity {
         public Long carId;
         public LocalDate startDay;
         public LocalDate enddDay;
+
+        @Override
+        public String toString() {
+            return "Reservation [id=" + id + ", carId=" + carId + ", startDay=" + startDay + ", enddDay=" + enddDay
+                    + ", userId=" + userId + "]";
+        }
+
         public String userId;
     }
 
     public Invoice(double totalPrice, boolean paid, Reservation reservation) {
-        this.id = null;
         this.totalPrice = totalPrice;
         this.paid = paid;
         this.reservation = reservation;
+    }
+
+    @Override
+    public String toString() {
+        return "Invoice [totalPrice=" + totalPrice + ", paid=" + paid + ", reservation=" + reservation + "]";
     }
 
 }
